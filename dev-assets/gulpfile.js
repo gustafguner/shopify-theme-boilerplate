cdcbe458cc9e8d15775011d3a04a3105e8e03c56
Gustaf Gunér <gustaf@guner.se>
2019-08-28 16:36:09 +0200

    Gulp sass task working

--- dev-assets/gulpfile.js
+++ dev-assets/gulpfile.js
@@ -11,3 +11,30 @@ const changed = require("gulp-changed");
 const t2 = require("through2");
 
 sass.compiler = require("node-sass");
+
+const ASSETS_FOLDER_PATH = "../assets/";
+
+gulp.task("sass", function() {
+  return gulp
+    .src("sass/**/*.scss.liquid")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        cascade: false
+      })
+    )
+    .pipe(rename("application.scss.liquid"))
+    .pipe(
+      t2.obj((chunk, enc, cb) => {
+        let date = new Date();
+        chunk.stat.atime = date;
+        chunk.stat.mtime = date;
+        cb(null, chunk);
+      })
+    )
+    .pipe(gulp.dest(ASSETS_FOLDER_PATH));
+});
+
+gulp.task("watch", function() {
+  gulp.watch("sass/**/*.{scss,liquid}", gulp.series("sass"));
+});
