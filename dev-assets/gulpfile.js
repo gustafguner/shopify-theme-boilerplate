841a951217611dd36e0dd46fc0687b0a280e4f4c
Gustaf Gunér <gustaf@guner.se>
2019-08-28 16:52:43 +0200

    Gulp js/js-vendors tasks

--- dev-assets/gulpfile.js
+++ dev-assets/gulpfile.js
@@ -35,6 +35,47 @@ gulp.task("sass", function() {
     .pipe(gulp.dest(ASSETS_FOLDER_PATH));
 });
 
+gulp.task("js", function() {
+  return gulp
+    .src("js/*.js")
+    .pipe(
+      babel({
+        presets: ["@babel/preset-env"]
+      })
+    )
+    .pipe(concat("application.js"))
+    .pipe(
+      t2.obj((chunk, enc, cb) => {
+        let date = new Date();
+        chunk.stat.atime = date;
+        chunk.stat.mtime = date;
+        cb(null, chunk);
+      })
+    )
+    .pipe(gulp.dest(ASSETS_FOLDER_PATH))
+    .pipe(rename("application.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest(ASSETS_FOLDER_PATH));
+});
+
+gulp.task("js-vendors", function() {
+  return gulp
+    .src("js/vendors/*.js")
+    .pipe(concat("vendors.min.js"))
+    .pipe(
+      t2.obj((chunk, enc, cb) => {
+        let date = new Date();
+        chunk.stat.atime = date;
+        chunk.stat.mtime = date;
+        cb(null, chunk);
+      })
+    )
+    .pipe(uglify())
+    .pipe(gulp.dest(ASSETS_FOLDER_PATH));
+});
+
 gulp.task("watch", function() {
   gulp.watch("sass/**/*.{scss,liquid}", gulp.series("sass"));
+  gulp.watch("js/*.js", gulp.series("js"));
+  gulp.watch("js/vendors/*.js", gulp.series("js-vendors"));
 });
